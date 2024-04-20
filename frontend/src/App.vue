2fc85a77b4e70bef70289f277652f5c43c13998a
Tyler Li <sh0risu179@outlook.com>
2024-04-20 01:29:09 -0700

    basic ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,15 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="">
+  <div class="fixed p-0 m-0 top-0 left-0 w-full h-full bg-surface-900">
+    <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </header>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,12 +4,12 @@ import AccordionTab from 'primevue/accordiontab'
 </script>
 
 <template>
-  <Accordion :active-index="0">
-    <AccordionTab header="Header 1">
-      <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda atque aut beatae dolor dolorem, eaque ipsam laboriosam libero natus nesciunt obcaecati officia optio quaerat qui ratione suscipit temporibus.</p>
-    </AccordionTab>
-    <AccordionTab header="Header 2">
-      <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda atque aut beatae dolor dolorem, eaque ipsam laboriosam libero natus nesciunt obcaecati officia optio quaerat qui ratione suscipit temporibus.</p>
-    </AccordionTab>
-  </Accordion>
+    <Accordion :active-index="0">
+      <AccordionTab header="Header 1">
+        <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda atque aut beatae dolor dolorem, eaque ipsam laboriosam libero natus nesciunt obcaecati officia optio quaerat qui ratione suscipit temporibus.</p>
+      </AccordionTab>
+      <AccordionTab header="Header 2">
+        <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda atque aut beatae dolor dolorem, eaque ipsam laboriosam libero natus nesciunt obcaecati officia optio quaerat qui ratione suscipit temporibus.</p>
+      </AccordionTab>
+    </Accordion>
 </template>
