4d8c16397ea962b6e37d21543bc5cd81eb40e743
Tyler Li <sh0risu179@outlook.com>
2024-04-20 00:40:36 -0700

    primevue package added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,14 @@ import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Lara from './presets/lara'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: Lara
+})
 app.mount('#app')
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Accordion from 'primevue/accordion'
+import AccordionTab from 'primevue/accordiontab'
+</script>
 
 <template>
-  <p class="text-amber-800">Hello world</p>
+  <Accordion :active-index="0">
+    <AccordionTab header="Header 1">
+      <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda atque aut beatae dolor dolorem, eaque ipsam laboriosam libero natus nesciunt obcaecati officia optio quaerat qui ratione suscipit temporibus.</p>
+    </AccordionTab>
+    <AccordionTab header="Header 2">
+      <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda atque aut beatae dolor dolorem, eaque ipsam laboriosam libero natus nesciunt obcaecati officia optio quaerat qui ratione suscipit temporibus.</p>
+    </AccordionTab>
+  </Accordion>
 </template>
