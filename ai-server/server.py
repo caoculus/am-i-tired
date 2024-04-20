9c84c3ddd7808b53ce7b34c5a3c0a5e7ea89a580
Jonathan Cao <caojiasen@gmail.com>
2024-04-20 16:22:49 -0700

    AI-side server prototype

--- ai-server/server.py
+++ ai-server/server.py
@@ -1,26 +1,29 @@
+#!/usr/bin/env python3
 import asyncio
+from datetime import datetime
+import torchvision
 from websockets.server import WebSocketServerProtocol, serve
 from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
 
 async def handle(websocket: WebSocketServerProtocol):
     try:
-        # Collect all of the data first
-        all_data = bytearray()
-        while True:
-            message = await websocket.recv()
-            assert isinstance(message, bytes), "Did not receive bytes"
-            if not message:
-                break
-            all_data += message
-        # TODO: pass this to the model and then stuff
-
+        filename = f"{datetime.now()}.webm"
+        with open(filename, "wb") as f:
+            while True:
+                message = await websocket.recv()
+                assert isinstance(message, bytes), "Did not receive bytes"
+                if not message:
+                    break
+                f.write(message)
+        res = torchvision.io.read_file(filename)
+        await websocket.send(f"Got tensor with shape {res.shape}")
     except ConnectionClosedOK:
         pass
     except ConnectionClosedError as e:
         print(e)
 
 async def main():
-    async with serve(handle, "localhost", 3000):
+    async with serve(handle, "localhost", 3001):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
