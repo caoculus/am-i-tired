152bee7e280463a9065ae518d533ce16561528bc
Tyler Li <sh0risu179@outlook.com>
2024-04-21 09:15:29 -0700

    text change

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import Accordion from 'primevue/accordion'
-import AccordionTab from 'primevue/accordiontab'
-import Panel from 'primevue/panel'
-</script>
+import Panel from 'primevue/pan</script>
 
 <template>
   <div class="flex flex-col m-0 position-absolute justify-center items-center">
     <Panel header="Welcome to our Project!">
         <p class="m-0">
-          This project was made to detect tiredness through a short video (under 2 second!).
+          This project was made to detect tiredness through a short video (under 5 seconds!).
           Switch to App tab to try it out yourself!
         </p>
     </Panel>
